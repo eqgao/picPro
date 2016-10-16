e2cb11f0f1be027f23ced49085cb636e7a892d67
eqgao <215160083@qq.com>
2016-10-17 02:36:39 +0800

    material design 侧滑 viewpager fragment

--- app/src/main/java/gdut/com/picpro/fragment/OneFragment.java
+++ app/src/main/java/gdut/com/picpro/fragment/OneFragment.java
@@ -3,11 +3,17 @@ package gdut.com.picpro.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import gdut.com.picpro.R;
 
 public class OneFragment extends Fragment {
 
+    @Nullable
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_one, container, false);
     }
 }
--- app/src/main/java/gdut/com/picpro/fragment/TwoFragment.java
+++ app/src/main/java/gdut/com/picpro/fragment/TwoFragment.java
@@ -1,10 +1,22 @@
 package gdut.com.picpro.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import gdut.com.picpro.R;
 
 /**
  * Created by hasee on 2016/10/16.
  */
 
 public class TwoFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
 }
